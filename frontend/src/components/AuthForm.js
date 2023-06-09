44e28ecbcc4da52102fd8589a51c5188e803d1c9
kjh-busan <picsmash7@gmail.com>
2023-06-09 18:58:48 +0900

    fix: authform

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,4 +1,10 @@
-import { Form, Link, useSearchParams, useActionData } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useSearchParams,
+  useActionData,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
@@ -34,7 +40,9 @@ function AuthForm() {
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disabled={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
