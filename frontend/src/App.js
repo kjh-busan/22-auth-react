70fb80b4c0ca3abff9704547c66afe1549526775
kjh-busan <picsmash7@gmail.com>
2023-07-13 21:47:02 +0900

    mod init

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,5 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from "./pages/EditEvent";
-import ErrorPage from "./pages/Error";
-import EventDetailPage, {
-  loader as eventDetailLoader,
-  action as deleteEventAction,
-} from "./pages/EventDetail";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventsRootLayout from "./pages/EventsRoot";
-import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
-import RootLayout from "./pages/Root";
-import { action as manipulateEventAction } from "./components/EventForm";
-import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([]);
 
