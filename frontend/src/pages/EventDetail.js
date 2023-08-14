9b654d84ed99879a4d27e976a3ddb7929d3cd4f9
kjh-busan <picsmash7@gmail.com>
2023-08-14 23:00:37 +0900

    revert

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -71,10 +71,7 @@ export async function loader({ request, params }) {}
 
 export async function action({ params, request }) {
   const eventId = params.eventId;
-  const response = await fetch("http://localhost:8080/events/" + eventId, {
-    method: request.method,
-    headers: {},
-  });
+
 
   if (!response.ok) {
     throw json(
