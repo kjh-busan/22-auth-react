cde803ed75bd972cc4c1c0e9322eef249be67114
kjh-busan <picsmash7@gmail.com>
2023-08-17 22:12:19 +0900

    delete files

--- frontend/src/pages/EventsRoot.js
+++
@@ -1,9 +0,0 @@
-import { Outlet } from 'react-router-dom';
-
-import EventsNavigation from '../components/EventsNavigation';
-
-function EventsRootLayout() {
-  return <>Todo</>;
-}
-
-export default EventsRootLayout;
