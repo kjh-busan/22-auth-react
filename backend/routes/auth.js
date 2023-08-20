dab8291610fde4d737b2287c964328f50b1457c1
kjh-busan <picsmash7@gmail.com>
2023-08-21 00:59:13 +0900

    revert auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,18 +1,5 @@
 const router = express.Router();
 
-router.post("/signup", async (req, res, next) => {
-  const data = req.body;
-  let errors = {};
-
-  if (!pwIsValid) {
-    return res.status(422).json({
-      message: "Invalid credentials.",
-      errors: { credentials: "Invalid email or password entered." },
-    });
-  }
-
-  const token = createJSONToken(email);
-  res.json({ token });
-});
+router.post("/signup", async (req, res, next) => {});
 
 module.exports = router;
