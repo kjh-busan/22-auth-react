43b5ff5c6d190b21f13b08524314f71fa13bc819
kjh-busan <picsmash7@gmail.com>
2023-08-21 21:22:32 +0900

    backend

--- backend/util/errors.js
+++ backend/util/errors.js
@@ -5,12 +5,5 @@ class NotFoundError {
   }
 }
 
-class NotAuthError {
-  constructor(message) {
-    this.message = message;
-    this.status = 401;
-  }
-}
-
 exports.NotFoundError = NotFoundError;
 exports.NotAuthError = NotAuthError;
