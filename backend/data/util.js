8e91e393ead873c348e799ec756c4291333a6317
kjh-busan <picsmash7@gmail.com>
2023-08-22 20:39:45 +0900

    revert 2

--- backend/data/util.js
+++ backend/data/util.js
@@ -1,9 +1,5 @@
 const fs = require('node:fs/promises');
 
-async function readData() {
-  const data = await fs.readFile('events.json', 'utf8');
-  return JSON.parse(data);
-}
 
 async function writeData(data) {
   await fs.writeFile('events.json', JSON.stringify(data));
